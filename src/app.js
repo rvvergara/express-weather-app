fb12f9fcca19b8d4706234bae8befaef8d34a403
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-10-30 16:15:07 +0800

    Set favicon and head partial

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use(express.static(publicDirPath));
 
 app.get('/', (req, res) => {
   res.render('index', {
-    title: 'Weather App',
+    title: 'Know The Weather',
     name: 'Ryan V',
   });
 });

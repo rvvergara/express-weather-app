d77aaebf6a0c957851bc9f350885631ad7a7378a
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-11-04 15:18:42 +0800

    Use fetch on client side app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,6 @@
-console.log('Client side JS is loaded');
+fetch('/weather?city=manila')
+  .then((res) => res.json())
+  .then(({ error, summary, location }) => (
+    error ? console.log(error) : console.log({ summary, location })
+  ))
+  .catch((e) => console.log(e));
